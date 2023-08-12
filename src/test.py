064b452f8970b7532293f78ff4152059cf2bc3d5
Aidan McGauley <40058902@ads.qub.ac.uk>
2023-08-12 14:40:00 +0100

    New test cases

--- src/test.py
+++ src/test.py
@@ -2,9 +2,22 @@ import unittest
 import addHours
 
 class TestAddHours(unittest.TestCase):
-    def test_add_hours(self):
-        attendances = [2,1,2,2]
-        self.assertEqual(addHours.add_hours(attendances),7)
+
+    def test_add_hours_normal(self):
+        attendances = [2, 1, 2, 2]
+        self.assertEqual(addHours.add_hours(attendances), 7)
+
+    def test_add_hours_empty(self):
+        attendances = []
+        self.assertEqual(addHours.add_hours(attendances), 0)
+
+    def test_add_hours_negative(self):
+        attendances = [2, -1, 2, 2]
+        self.assertEqual(addHours.add_hours(attendances), 5)
+
+    def test_add_hours_large_numbers(self):
+        attendances = [99999, 100000, 100000, 100001]
+        self.assertEqual(addHours.add_hours(attendances), 400000)
 
 if __name__ == '__main__':
     unittest.main()
